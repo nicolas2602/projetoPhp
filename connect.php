6ab03271f1f1f709a2e12585c210ae28c384942f
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2021-04-29 17:08:10 -0300

    PHP log parte 2

--- connect.php
+++ connect.php
@@ -1,6 +1,9 @@
 <?php 
 
-session_start();
+if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
 
 //                      servidor, usuario, senha, nome do banco de dados.
 $con=  mysqli_connect("localhost","root","","test")
