a57e7afed4433553c56453adeaf6b3970899bf90
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2021-04-29 12:29:55 -0300

    PHP log 3°T.I
    Nicolas Hiroki Yonekawa 3°T.I

---
+++ logger.sql
@@ -0,0 +1,10 @@
+   Create table logging(
+        IdLogging int primary key AUTO_INCREMENT,
+        dateLogging datetime not null,
+        level varchar(100),
+        msg varchar(100) not null,
+        fk_reg int not null,
+        FOREIGN key (fk_reg) references reg (id)   
+    );
+
+    insert into logging (dateLogging, level, msg, fk_reg) VALUES (now(), 'INFO', 'Teste', 1);
